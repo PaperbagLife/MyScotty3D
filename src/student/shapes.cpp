309b76feb8f449a9bb578beb6dcf24b9e3ea29e6
Ricky <yunkunl@andrew.cmu.edu>
2021-10-31 21:37:23 -0400

    sphere

--- src/student/shapes.cpp
+++ src/student/shapes.cpp
@@ -24,9 +24,32 @@ Trace Sphere::hit(const Ray& ray) const {
     // ray.dist_bounds! For example, if there are two intersections,
     // but only the _later_ one is within ray.dist_bounds, you should
     // return that one!
-
     Trace ret;
     ret.origin = ray.point;
+    Vec3 o = ray.point;
+    Vec3 d = ray.dir;
+    float r2 = radius * radius;
+    float od = dot(o, d);
+    float t1 = -od + sqrt(od*od - o.norm_squared() + r2);
+    float t2 = -od - sqrt(od*od - o.norm_squared() + r2);
+    if (t1 > ray.dist_bounds.x && t1 < ray.dist_bounds.y) {
+        // can intersect
+        ray.dist_bounds.x = t1;
+        ret.hit = true;
+        ret.position = o + t1*d;
+        ret.distance = (t1*d).norm();
+        ret.normal = ret.position.unit();
+        return ret;
+    }
+    if (t2 > ray.dist_bounds.x && t2 < ray.dist_bounds.y) {
+        // can intersect
+        ray.dist_bounds.x = t2;
+        ret.hit = true;
+        ret.position = o + t2*d;
+        ret.distance = (t2*d).norm();
+        ret.normal = ret.position.unit();
+        return ret;
+    }
     ret.hit = false;       // was there an intersection?
     ret.distance = 0.0f;   // at what distance did the intersection occur?
     ret.position = Vec3{}; // where was the intersection?
