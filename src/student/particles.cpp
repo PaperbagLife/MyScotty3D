1f7849aa03e7dc1cbb641cd658dad26848786776
Ricky <yunkunl@andrew.cmu.edu>
2021-11-26 23:14:56 -0500

    task4

--- src/student/particles.cpp
+++ src/student/particles.cpp
@@ -18,6 +18,27 @@ bool Scene_Particles::Particle::update(const PT::Object& scene, float dt, float 
     // (4) Repeat until the entire time step has been consumed.
 
     // (5) Decrease the particle's age and return whether it should die.
-
-    return false;
+    while (dt > 0.0f) {
+        Ray ray = Ray(pos, velocity);
+        PT::Trace trace = scene.hit(ray);
+        float costheta = dot(trace.normal, -velocity)/(velocity.norm()*trace.normal.norm());
+        float theta = acos(costheta);
+        float extendX = fabsf(tan(theta) * radius);
+        if (!trace.hit || trace.distance > velocity.norm()*dt + extendX ) {
+            pos += velocity * dt;
+            velocity += acceleration * dt;
+            age -= dt;
+            return age > 0.0f;
+        }
+        // hit
+        float hitTime = (trace.distance - extendX)/velocity.norm();
+        if(trace.distance - extendX > 0.0f) {
+            pos += velocity * hitTime;
+        }
+        velocity = 2.0f*(dot(trace.normal, -velocity)*trace.normal) + velocity;
+        velocity += acceleration * hitTime;
+        dt -= hitTime;
+        age -= hitTime;
+    }
+    return age > 0.0f;
 }
